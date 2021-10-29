1be98bc428a08169da959a524b0b2a5e794bd2ae
gauravsinhaweb <sinhagaaurav37@gmail.com>
2021-10-29 21:48:09 +0530

    🚀 project begins

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "./pages/Hero";
+
+function App() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <>
+      <div>Get Your</div>
+      <div>handwritten notes</div>
+      <div>real code</div>
+      <div>
+        <button>Get Started</button>
+      </div>
+    </>
+  );
+}
+
+export default Hero;
