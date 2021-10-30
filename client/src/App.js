3dfe8cfd0a6562b23532572e54d6e8ed774c45e4
gauravsinhaweb <sinhagaaurav37@gmail.com>
2021-10-30 16:32:50 +0530

    ✨ created route on app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import History from "./pages/History";
+import InputCode from "./pages/InputCode";
+import Login from "./pages/Login";
+import OutputCode from "./pages/OutputCode";
+import Profile from "./pages/Profile";
 import TakePhoto from "./pages/TakePhoto";
 
 function App() {
@@ -11,6 +16,24 @@ function App() {
           <Switch>
             <Route path="/" exact>
               <TakePhoto />
+            </Route>
+
+            <Route path="/input" exact>
+              <InputCode />
+            </Route>
+
+            <Route path="/output" exact>
+              <OutputCode />
+            </Route>
+
+            <Route path="/login" exact>
+              <Login />
+            </Route>
+            <Route path="/history" exact>
+              <History />
+            </Route>
+            <Route path="/profile" exact>
+              <Profile />
             </Route>
           </Switch>
         </Router>
---
+++ client/src/pages/OutputCode.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function OutputCode() {
+  return <div>output</div>;
+}
+
+export default OutputCode;
