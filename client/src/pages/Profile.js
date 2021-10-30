4bbcddc55be3bed54f93d4b2d0a7586f95b0b837
Amoghtech <amogh.1923cs1192@kiet.edu>
2021-10-30 22:38:07 +0530

    Revert "✨ Add Login and Profile page"
    This reverts commit 151dc288db01d75900ac812fee883b86928fbaa4.

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,28 +1,7 @@
 import React from "react";
-import firebase from "firebase"
-import { AiFillCaretLeft } from "react-icons/ai";
-import { Link } from "react-router-dom"
 
 function Profile() {
-  return (
-    <div className="h-screen w-full">
-      <div className="flex w-full">
-        <Link to="/" className="pt-8" >
-          {" "}
-          <AiFillCaretLeft />
-        </Link>
-        <h1 className="text-center pt-4 text-3xl pl-32">Profile</h1>
-      </div>
-      <div className="grid place-items-center w-full">
-        <img className="rounded-full h-44 w-44 my-8"
-          alt="profile picture"
-          src={firebase.auth().currentUser.photoURL}
-        />
-        <h1 className="text-2xl">{firebase.auth().currentUser.displayName}</h1>
-        <h1 className="text-2xl pt-56">{firebase.auth().currentUser.email}</h1>
-      </div>
-    </div>
-  );
+  return <div>profile</div>;
 }
 
 export default Profile;
