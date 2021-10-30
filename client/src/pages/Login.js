4a802e4d59d7eb7b1156b113e457c794fbbb0d6d
Your Name <hr5359@srmist.edu.in>
2021-10-31 00:13:01 +0530

    ✨ Add profile page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,58 +1,7 @@
-import React, { Component } from "react"
-// import "./App.css"
-import firebase from "firebase"
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
-// import Main from "./main"
+import React from "react";
 
-firebase.initializeApp({
-  apiKey: "AIzaSyCjF1NGOgCvc4YgRvvLH-pSGaJGsCPudIM",
-  authDomain: "codemirror-fe6c6.firebaseapp.com"
-})
-
-class Login extends Component {
-  state = { isSignedIn: false }
-  uiConfig = {
-    signInFlow: "popup",
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID
-    ],
-    callbacks: {
-      signInSuccess: () => false
-    }
-  }
-
-  componentDidMount = () => {
-    firebase.auth().onAuthStateChanged(user => {
-      this.setState({ isSignedIn: !!user })
-      console.log("user", user)
-    })
-  }
-
-  render() {
-    return (
-      <div className="Login">
-        {this.state.isSignedIn ? (
-          // Add your main component here
-          <div>
-          </div>
-          // <Main />
-        ) : (
-          <div>
-            <p className="grid place-items-center h-full w-full text-2xl font-bold pt-6">Welcome</p>
-            <p className="grid place-items-center h-full w-full text-xl font-bold pt-36">Login</p>
-            <div className="m-4 border-2 border-grey-600 py-12 rounded-sm">
-              <StyledFirebaseAuth
-                uiConfig={this.uiConfig}
-                firebaseAuth={firebase.auth()}
-              />
-            </div>
-            <p className="grid place-items-center mb-0 pt-64">Made with ❤️ by Hack Elite</p>
-          </div>
-        )}
-      </div>
-    )
-  }
+function Login() {
+  return <div>Login</div>;
 }
 
-export default Login
+export default Login;
